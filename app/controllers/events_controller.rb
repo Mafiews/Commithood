ed7d76fedf3a40cb04bbbf28648e24e0cb091f11
Yizhu ZONG <agnes.lain@gmail.com>
2020-08-25 12:54:15 +0200

    homepage and index segmentation done, wait for data to create the final view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,9 @@
 class EventsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
 
   # yizhu: add index controler to show all events
   def index
-    @events = Events.all
+    @events = Event.all
     # authorize @events
      # @events = policy_scope(Event)
   end
