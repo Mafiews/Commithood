0dc4f7f7d644f7b75e5b487139d5a0b9b211dc4d
ilana mrejen <ilana.mrejen@gmail.com>
2020-08-25 16:44:58 +0200

    Add dotenv - Protect my secret data in .env file

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,21 +1,24 @@
 class EventsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_event, only: [:show, :destroy]
-  
+
   # yizhu: add index controler to show all events
   def index
     @events = Event.all
     # authorize @events
      # @events = policy_scope(Event)
   end
-  
-  #Ilana 
+
+  #Ilana
   def show
     set_event
   end
 
   private
+
   #ilana
+
+
   def set_event
     @event = Event.find(params[:id])
   end
