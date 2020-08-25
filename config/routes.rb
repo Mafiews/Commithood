614475c5d1a9002c51938146b426dc8a43e7e039
ilana mrejen <ilana.mrejen@gmail.com>
2020-08-25 11:19:22 +0200

    rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :events, only: [:destroy, :show, :index, :create] do
+    resources :participation, only: [:create]
+  end
+
+  get 'dashboard', to: 'pages#dashboard'
 end
