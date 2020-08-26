ca82fbf6f169e30a2cc5d58a06e04143d41b2f65
Kallyanny Bun <kallyannybun@gmail.com>
2020-08-26 18:11:43 +0200

    added upcoming and past event participations in dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,18 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    current_user
-    # @my_events = current_user.events
+    @my_events = current_user.events
+    # Kally : set variables to filter @my_events
+    @my_upcoming_events = []
+    @my_past_events = []
+    @my_events.each do |my_event|
+      if my_event.end_date < Date.today
+        @my_past_events << my_event
+      else
+        @my_upcoming_events << my_event
+      end
+    end
+    @my_upcoming_events
+    @my_past_events
   end
 end
--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,6 @@
 class Participation < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  validates :event, uniqueness: { scope: :user }
 end
