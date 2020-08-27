44799c4da7dc48a42e15075d26189e79081c9207
Kallyanny Bun <kallyannybun@gmail.com>
2020-08-27 12:31:57 +0200

    count seats_left in events and participations controllers + displayed on index cards + logo

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,8 +4,10 @@ class Event < ApplicationRecord
   has_many :participations
   has_many :users, through: :participations
   validates :name, :start_date, :end_date, :description, :seats, :address, presence: true
+  # Kally : seats validation
+  validates :seats, numericality: { greater_than: 0 }
+  validates :seats_left, numericality: { greater_than_or_equal_to: 0 }
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-
 end
