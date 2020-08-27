44ff1c18db800db3ddde2a6c10bd378715545dcf
Yizhu ZONG <agnes.lain@gmail.com>
2020-08-27 14:32:20 +0200

    add index filter for the home searchbar, but can't test without the cause event link table, may have erros for index, I will fix after

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,23 @@ class EventsController < ApplicationController
   # yizhu: add index controler to show all events
   def index
     #Matt for pundit ?
-    @events = policy_scope(Event).geocoded.order(created_at: :desc)
+    @address = params[:address]
+    @events = policy_scope(Event).includs(:causes)
+
+    if params[:user_cause].present? && (params[:user_cause] != "Tous les thèmes") && (params[:address].present?)
+      @causes = params[:user_cause]
+      @events = filter_events_address_cause(@events, @causes, @address)
+
+    elsif params[:user_cause].present? && (params[:user_cause] != "Tous les thèmes")
+      @causes = params[:user_cause]
+      @events = filter_events_cause(@events, @causes)
+
+    elsif params[:address].present?
+      @events = @events.near(@address, 5)
+
+    else
+      @events = policy_scope(Event).geocoded.order(created_at: :desc)
+    end
 
     @markers = @events.map do |event|
       {
@@ -28,5 +44,21 @@ class EventsController < ApplicationController
   def set_event
     @event = Event.find(params[:id])
     authorize @event
+  end
+
+  def filter_events_address_cause(events, causes, address)
+    filter_events = []
+      causes.each do |cause|
+        events.near(@address, 5).select {|event| select_event << event.causes.includs?(cause)}
+      end
+    filtered_events
+  end
+
+  def filter_events_cause(events, causes)
+    filter_events = []
+      causes.each do |cause|
+        @events.where(causes: cause)
+      end
+    filtered_events
   end
 end
