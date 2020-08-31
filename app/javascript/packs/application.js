86b3896bdc1005c6bac018c597abfa0a100328a8
ilana mrejen <ilana.mrejen@gmail.com>
2020-08-31 17:06:19 +0200

    navbar transparence and fancy effect

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,11 +34,14 @@ import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { initSweetalert } from '../plugins/init_sweetalert';
 import { initSelect2 } from '../plugins/init_select2';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initAutocomplete();
+  initUpdateNavbarOnScroll()
   initSelect2();
   initSweetalert('#sweet-alert-register', {
     title: "Participation confirmée",
