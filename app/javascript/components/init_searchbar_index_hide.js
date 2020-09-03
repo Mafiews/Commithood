1a2721f44e10fcc066eb60b0278f356cc3c62a47
ilana mrejen <ilana.mrejen@gmail.com>
2020-09-03 10:51:08 +0200

    j'ai changé la couleur de la flèche du caroussel

--- app/javascript/components/init_searchbar_index_hide.js
+++ app/javascript/components/init_searchbar_index_hide.js
@@ -2,17 +2,19 @@ const initSearchbarIndexHide = () => {
   const button = document.querySelector('#button-menu i');
   const menu = document.querySelector('.search-bar-index');
   console.log(menu);
-  button.addEventListener('click',(event)=>{
-    if (button.classList.contains('fa-caret-square-left')) {
-      button.classList.remove("fa-caret-square-left");
-      button.classList.add("fa-caret-square-right");
-      menu.classList.add('inactive');
-    } else {
-      button.classList.remove("fa-caret-square-right");
-      button.classList.add("fa-caret-square-left");
-      menu.classList.remove('inactive');
-    };
-  });
+  if(button) {
+    button.addEventListener('click',(event) => {
+      if (button.classList.contains('fa-caret-square-left')) {
+        button.classList.remove("fa-caret-square-left");
+        button.classList.add("fa-caret-square-right");
+        menu.classList.add('inactive');
+      } else {
+        button.classList.remove("fa-caret-square-right");
+        button.classList.add("fa-caret-square-left");
+        menu.classList.remove('inactive');
+      };
+    });
+    }
 }
 
 export {initSearchbarIndexHide};
