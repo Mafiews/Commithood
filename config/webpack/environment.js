8ec1fa9bcf1802842a33b48b73d790458dd47fe3
Kallyanny Bun <kallyannybun@gmail.com>
2020-09-03 09:06:52 +0200

    fixed some css and bookmark redirection

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,17 +1,7 @@
-const { environment } = require('@rails/webpacker')
-
-const webpack = require('webpack');
+// config/webpacker/environment.js
+const { environment } = require('@rails/webpacker');
 
 // Preventing Babel from transpiling NodeModules packages
 environment.loaders.delete('nodeModules');
 
-// Bootstrap 4 has a dependency over jQuery & Popper.js:
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-);
-
-module.exports = environment
+module.exports = environment;
