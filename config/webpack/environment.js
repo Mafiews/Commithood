d9bb01805c905479733997d9e3da513675a28229
Kallyanny Bun <kallyannybun@gmail.com>
2020-09-03 09:11:14 +0200

    changed environnement.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,18 @@
 // config/webpacker/environment.js
 const { environment } = require('@rails/webpacker');
 
+const webpack = require('webpack');
+
 // Preventing Babel from transpiling NodeModules packages
 environment.loaders.delete('nodeModules');
+
+// Bootstrap 4 has a dependency over jQuery & Popper.js:  module.exports = environment;
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+);
 
 module.exports = environment;
