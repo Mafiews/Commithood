f2c6dcc09c6ae2e08c263855eb235c205dce6bea
Yizhu ZONG <agnes.lain@gmail.com>
2020-09-03 22:45:30 +0200

    removed unused console.logs

--- app/javascript/components/init_searchbar_index.js
+++ app/javascript/components/init_searchbar_index.js
@@ -3,7 +3,6 @@ const initSearchbarIndex = () => {
   const causes = document.querySelectorAll('#user_cause');
   causes.forEach ((cause) => {
     cause.addEventListener('click', (event)=> {
-      console.log(cause)
       if (cause.value === "Tous les thèmes" && cause.checked === true) {
         causes.forEach ((c) => {
           c.checked = false
